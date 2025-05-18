08da4eaa9fdb6452e330c565456be2c9a56a1475
Alexander <alexandervolzhanin2004@gmail.com>
2025-05-18 23:48:59 +0300

    add api

--- main.py
+++ main.py
@@ -8,46 +8,58 @@ from app.rag import (
     RAGPipeline,
 )
 from dotenv import load_dotenv, find_dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+load_dotenv(find_dotenv(raise_error_if_not_found=True))
+
+config = Config(
+    PROXY=os.getenv("PROXY"), OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+)
+
+# Инициализация компонентов
+file_loader = LangChainFileLoader(
+    data_for_rag="data_for_rag", show_progress=True
+)
+text_splitter = LangChainTextSplitter()
+document_store = LangChainOpenAIEmbeddings()
+
+# Сборка пайплайна
+pipeline = RAGPipeline(file_loader, text_splitter, document_store)
+doc_store = pipeline.execute()
+
+memory_manager = MemoryManager()
+llm = OpenAILanguageModel(memory_manager, doc_store)
+
+
+class Question(BaseModel):
+    question: str
+
+
+app = FastAPI(
+    title="API для получения ответа от LLM вместе с RAG",
+    description="API для получения ответа от LLM вместе с RAG и историей сообщений.",
+    version="1.0.0"
+)
+
+@app.get("/health", summary="Проверка состояния")
+def health_check():
+    return {
+        "status": "ok"
+    }
+
+@app.post("/ask", summary="Получения ответа от LLM")
+def ask_question(question: Question):
+    llm = OpenAILanguageModel(memory_manager, doc_store)
+    result = llm.generate_response(
+        f"Запрос пользователя: {question}. Предыдущие сообщения: {memory_manager.get_history()}"
+    )
+    return result["result"], memory_manager.get_history()
 
 
 def main():
-
-    load_dotenv(find_dotenv(raise_error_if_not_found=True))
-
-    config = Config(
-        PROXY=os.getenv("PROXY"), OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
-    )
-
-    # Инициализация компонентов
-    file_loader = LangChainFileLoader(
-        data_for_rag="data_for_preprocess", show_progress=True
-    )
-    text_splitter = LangChainTextSplitter()
-    document_store = LangChainOpenAIEmbeddings()
-
-    # Сборка пайплайна
-    pipeline = RAGPipeline(file_loader, text_splitter, document_store)
-    doc_store = pipeline.execute()
-
-    memory_manager = MemoryManager()
-    llm = OpenAILanguageModel(memory_manager, doc_store)
-
-    # Цикл запросов
-    while True:
-        try:
-            question = input("Ваш вопрос: ")
-            if not question:
-                continue
-            result = llm.generate_response(
-                f"Запрос пользователя: {question}. Предыдущие сообщения: {memory_manager.get_history()}"
-            )
-            print(f"Ответ: {result["result"]}")
-            print(f"\n{memory_manager.get_history()}")
-        except Exception as err:
-            print(f"Произошла ошибка: {err}")
-        except KeyboardInterrupt:
-            print("\nЗавершение программы...")
-            break
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
